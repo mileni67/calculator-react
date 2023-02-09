6730542c00ec1ef1e0dfcf2da71e8d154649c22b
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-02-09 19:47:06 +0100

    changes clock function

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,4 +1,14 @@
-import { useEffect, useState } from "react";
+import "./style.css";
+import { useState, useEffect } from "react";
+
+const formatDate = (date) => date.toLocaleString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+});
 
 export const Clock = () => {
     const [date, setDate] = useState(new Date());
@@ -6,21 +16,21 @@ export const Clock = () => {
     useEffect(() => {
         const intervalId = setInterval(() => {
             setDate(new Date());
-        }, 1000)
+        }, 1000);
 
         return () => {
-            clearInterval(intervalId);
+            clearInterval(intervalId)
         };
-
     }, []);
 
-    <p className="clock">
-        Dzisiaj jest {" "}
-        {date.toLocalString(undefined, {
-            weekday: "long",
-            day: "numeric",
-            month: "long"
-        })}
-    </p>
-
+    return (
+        <div className="clock">
+            Dzisiaj jest
+            {" "}
+            {formatDate(date)}
+        </div>
+    );
 };
+
+export default Clock;
+
